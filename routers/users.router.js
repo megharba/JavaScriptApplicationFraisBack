5b6033ebfe29e2dcc0831d0ebf499a0917be8374
megharba <b.megharba@gmail.com>
2021-05-23 18:45:25 +0200

    commit router

--- routers/users.router.js
+++ routers/users.router.js
@@ -8,7 +8,7 @@ router.post('/auth', userController.searchAll)
 //un Utilisateur 
 router.get('/:id', userController.showOneUser)
 //les Utilisateur 
-router.get('/all/:id', userController.showAllUser)
+router.get('/all', userController.showAllUser)
 // //ShowOneUser
 // router.get('/show/:id',(request, response) => {
 //     const id = request.params.id
